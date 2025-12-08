8e381e36d89acd0ef48ec2598113d802521e939d
AkashJadon <jadonakash672@gmail.com>
2025-12-08 22:51:47 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary(){
@@ -118,6 +119,8 @@ export function renderOrderSummary(){
 
         container.remove();
         updateHeaderQuantity();
+        renderPaymentSummary();
+        
       });
     });
 
@@ -151,6 +154,7 @@ export function renderOrderSummary(){
 
               updateQuantity(productId, newQuantity);
               renderOrderSummary();
+              renderPaymentSummary();
               
             });
         });
@@ -163,6 +167,7 @@ export function renderOrderSummary(){
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
 
