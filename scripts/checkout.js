e9bedc4db6feb53ef469adc55165edd6370080ce
AkashJadon <jadonakash672@gmail.com>
2025-12-13 18:34:49 +0530

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {updateHeaderQuantity} from "./header.js";
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
