641777baf89c5460b136b1d1cdec5599c3098986
AkashJadon <jadonakash672@gmail.com>
2025-12-15 21:57:51 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,12 @@
-import {renderOrderSummary } from "./checkout/orderSummary.js";
-import {renderPaymentSummary} from "./checkout/paymentSummary.js";
-import {updateHeaderQuantity} from "./header.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { updateHeaderQuantity } from "./header.js";
 import '../data/cart-class.js';
-import '../data/backend-practice.js'
+// import '../data/backend-practice.js'
+import { loadProducts } from "../data/products.js";
 
-
-renderOrderSummary();
-renderPaymentSummary();
-updateHeaderQuantity();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  updateHeaderQuantity();
+});
