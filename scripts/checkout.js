8d4f6ff3ec759aec09448095be830f4a255381c6
AkashJadon <jadonakash672@gmail.com>
2025-12-15 21:18:12 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import {renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {updateHeaderQuantity} from "./header.js";
 import '../data/cart-class.js';
+import '../data/backend-practice.js'
+
 
 renderOrderSummary();
 renderPaymentSummary();
