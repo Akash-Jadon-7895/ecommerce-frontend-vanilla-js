d5668f82ad395b9b6ff490310c6b344bec63f16d
AkashJadon <jadonakash672@gmail.com>
2025-12-24 19:06:19 +0530

    Pass the orderid and productid of the product to the tracking page to track it

--- scripts/orders-page.js
+++ scripts/orders-page.js
@@ -93,7 +93,7 @@ async function renderOrderPage() {
         </div>
 
         <div class="product-actions">
-          <a href="tracking.html">
+         <a href="tracking.html?orderId=${order.id}&productId=${product.productId}">
             <button class="track-package-button button-secondary">
               Track package
             </button>
